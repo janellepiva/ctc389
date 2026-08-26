2afbd5388c69128fdd557604732b28f535acb456
JP <janelle.piva@lausd.net>
2026-08-26 23:35:36 +0000

    Lab 5

---
+++ jpivalab5.py
@@ -0,0 +1,28 @@
+#Janelle Piva
+#Lab 5
+
+nums = [22,24,26,28,30,32,34,36,38,40]
+
+print("No Loop")
+
+print(nums[0])
+print(nums[1])
+print(nums[2])
+print(nums[3])
+print(nums[4])
+print(nums[5])
+print(nums[6])
+print(nums[7])
+print(nums[8])
+print(nums[9])
+
+print("With Loop")
+
+for i in nums:
+    print(i)
+
+foods = ["apples","oranges","grapes","watermelons","tomatoes"]
+
+for i in foods:
+    print(i)
+

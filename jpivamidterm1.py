33940800fe1977664a34ff88d59afec7d6627e3b
JP <janelle.piva@lausd.net>
2026-09-10 00:17:05 +0000

    Midterm Program 1

---
+++ jpivamidterm1.py
@@ -0,0 +1,11 @@
+#Janelle Piva
+#Midterm 1
+
+name = input("Enter your name: ")
+number = int(input("Enter a number:"))
+
+add_number = int(input("How much to add to that number?: "))
+
+total = number + add_number
+
+print(name, "you entered", number, "and added", add_number, "to get a total of", total)
